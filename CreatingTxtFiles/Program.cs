2b1e37b541480afbd83a8294dd66319c2f83ee5e
kbworkshop <patricio.vega1925@gmail.com>
2019-01-31 18:14:00 -0300

    Add project files.

---
+++ CreatingTxtFiles/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingTxtFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 100 ;i++)
+            {
+                LogWriter(i.ToString());
+            }
+        }
+
+        /// <summary>
+        /// write lines in the log file.
+        /// </summary>
+        /// <param name="logMessage"></param>
+        public static void LogWriter(string logMessage)
+        {
+            try
+            {
+                string path = Util.GetPath();
+
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    Log(logMessage, sw);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        /// <summary>
+        /// create a new line log in the file.
+        /// </summary>
+        /// <param name="logMessage"></param>
+        /// <param name="txtWriter"></param>
+        public static void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.WriteLine(logMessage + ";" + DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ CreatingTxtFiles/Util.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingTxtFiles
+{
+    public class Util
+    {
+
+        /// <summary>
+        /// return path of file and the file name
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPath()
+        {
+            string path = string.Empty;
+
+            try
+            {
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd") + "_LOG";
+                fileName = fileName.Replace("-", "_");
+
+                path = @"C:\Users\rvargas\Desktop\FolderProject\" + fileName + ".txt";
+
+                if (!System.IO.File.Exists(path))
+                {    //if file dont exist then create the file log
+                    System.IO.File.Create(path);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return path;
+        }
+    }
+}
